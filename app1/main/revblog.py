6ded000dbe566516c143b49400460a5f2a51b0b2
runningsparrow <runningsparrow@gmail.com>
2019-04-15 17:23:54 +0800

    revblog

---
+++ app1/main/revblog.py
@@ -0,0 +1,32 @@
+from flask import render_template
+from flask import request,Response
+import json
+#导入蓝本 main
+from . import main
+from app1.models import Blog
+from app1 import db
+
+@main.route('/revblog', methods=('GET','POST'))
+def revblog():
+    if request.method == 'POST':
+        print("POST")
+        return render_template("revblog.html")
+    if request.method == 'GET':
+        print("GET")
+        blogid = request.args.get("blogid")
+        if blogid == None:
+            pass
+            return render_template("revblog.html")
+        else:
+            print(blogid)
+            #query 
+            data = Blog.query.filter_by(blodid=blogid).first()
+            print(data)
+            print(data.blogtopic)
+            context={
+                'blogid':data.blodid,
+                'blogtopic':data.blogtopic,
+                'blogcontent':data.blogcontent
+            }
+            return render_template("revblog.html",**context)
+        
