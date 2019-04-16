a175eb99983b7d0ea89a3c859631d7d1b60e9ef0
runningsparrow <runningsparrow@gmail.com>
2019-04-16 22:04:47 +0800

    rev

--- app1/main/revblog.py
+++ app1/main/revblog.py
@@ -11,23 +11,48 @@ def revblog():
     if request.method == 'POST':
         print("POST")
         # return render_template("revblog.html")
-        blogid = request.args.get("blogid")
-        if blogid == None:
-            pass
-            return null
-        else:
-            print(blogid)
-            #query 
-            data = Blog.query.filter_by(blodid=blogid).first()
-            print(data)
-            print(data.blogtopic)
-            context={
-                'blogid':data.blodid,
-                'blogtopic':data.blogtopic,
-                'blogcontent':data.blogcontent
-            }
-            return Response(json.dumps(context))
-            # return render_template("revblog.html",**context)
+        #获取表单数据
+        print(request.form.get("blogid"))
+        if request.form.get("blogid") == None:  
+            #处理非表单提交
+            print(request.args.get("blogid"))
+            blogid = request.args.get("blogid")
+            if blogid == None:
+                pass
+                return null
+            else:
+                print(blogid)
+                #query 
+                data = Blog.query.filter_by(blodid=blogid).first()
+                print(data)
+                print(data.blogtopic)
+                context={
+                    'blogid':data.blodid,
+                    'blogtopic':data.blogtopic,
+                    'blogcontent':data.blogcontent
+                }
+                return Response(json.dumps(context))
+        else:        
+            #处理表单提交,更新数据
+            blogid = request.form.get("blogid")
+            if blogid == None:
+                pass
+                return null
+            else:
+                print(blogid)
+                #update
+                
+                #query 
+                data = Blog.query.filter_by(blodid=blogid).first()
+                data.blogtopic = request.form.get("blogtopic")
+                data.blogcontent = request.form.get("blogcontent")
+                db.session.commit()
+                context={
+                    'blogid':data.blodid,
+                    'blogtopic':data.blogtopic,
+                    'blogcontent':data.blogcontent
+                }
+                return render_template("revblog.html",**context)
 
     if request.method == 'GET':
         print("GET")
