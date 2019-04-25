f9b22926091a61b48c17e993fe6026bcddea8e0f
runningsparrow <runningsparrow@gmail.com>
2019-04-26 00:21:03 +0800

    tree

--- app1/static/libs/js/base.js
+++ app1/static/libs/js/base.js
@@ -8,13 +8,6 @@ var tree = [
 
         tags: ['4'],
         
-        state: { // 初始化的状态（支持 4 种）
-            checked: true, // 是否可勾选
-            disabled: false, // 是否可用
-            expanded: true, // 是否可折叠
-            selected: false // 是否可选中
-        },
-
         nodes: [
 
           {
@@ -25,23 +18,10 @@ var tree = [
 
             tags: ['2'],
 
-            state: { // 初始化的状态（支持 4 种）
-                checked: true, // 是否可勾选
-                disabled: false, // 是否可用
-                expanded: true, // 是否可折叠
-                selected: false // 是否可选中
-            },
-
             nodes: [
 
               {
-                state: { // 初始化的状态（支持 4 种）
-                    checked: true, // 是否可勾选
-                    disabled: false, // 是否可用
-                    expanded: true, // 是否可折叠
-                    selected: false // 是否可选中
-                },
-
+                
                 text: 'Grandchild 1',
 
                 href: '#grandchild1',
@@ -104,7 +84,7 @@ var tree = [
 
         href: '#parent4',
 
-        tags: ['0']
+         tags: ['0']
 
       },
 
@@ -112,27 +92,124 @@ var tree = [
 
         text: 'Parent 5',
 
-        href: '#parent5'  ,
+        href: '#parent5',
 
-        tags: ['0']
+         tags: ['0']
 
-      }
+      },
+
   ];
 
-  function getTree() {
-    // Some logic to retrieve, or generate tree structure
-    return tree;
-  }
+function getTree() {
+  // Some logic to retrieve, or generate tree structure
+  return tree;
+}
 
 $(document).ready(
     $('#tree').treeview({
         data: getTree(),
-        backColor: "#FFFFFF",
-        color: "#428bca",
-        enableLinks: true,
-        collapseIcon: 'glyphicon glyphicon-flash', // 折叠图标，默认 min
-        expandIcon: 'glyphicon glyphicon-earphone', // 展开图标，默认 plus
+        // backColor: "#FFFFFF",
+        // color: "#428bca",
+        // enableLinks: true,
+        // collapseIcon: 'glyphicon glyphicon-flash', // 折叠图标，默认 min
+        // expandIcon: 'glyphicon glyphicon-earphone', // 展开图标，默认 plus
         
     }),
+
+    $('#tree').on('nodeSelected',function(event, data) { 
+      // 事件代码... 
+      if(data.state.expanded){ 
+        //处于展开状态则折叠 
+        $('#tree').treeview('collapseNode', data.nodeId); 
+      } 
+      else { 
+        //展开 
+        $('#tree').treeview('expandNode', data.nodeId); 
+      } 
+    }),
     
+    // $('#tree').on('nodeUnselected',function(event, data) { 
+    //   // 事件代码... 
+    //   if(data.state.expanded){ 
+    //     //处于展开状态则折叠 
+    //     $('#tree').treeview('collapseNode', data.nodeId,); 
+    //   } 
+    //   else { 
+    //     //展开 
+    //     $('#tree').treeview('expandNode', data.nodeId); 
+    //   } 
+    // })
+
 );
+
+$(document).on('click','#adropindex',function(){
+  $.ajax({
+      url: "http://localhost:5000/",
+      type: "GET",
+      // contentType: 'application/json',
+      // data:"{}",
+      // dataType: "JSON",
+      success:function(data){
+          console.log(data)
+          window.location.href="http://localhost:5000/";
+      },
+      error:function(jqXHR, textStatus, errorThrown){
+          console.log(errorThrown)
+          alert("内容获取错误")
+      }
+    });
+});
+
+$(document).on('click','#adropaddblog',function(){
+  $.ajax({
+      url: "http://localhost:5000/addblog",
+      type: "GET",
+      // contentType: 'application/json',
+      // data:"{}",
+      // dataType: "JSON",
+      success:function(data){
+          console.log(data)
+          window.location.href="http://localhost:5000/addblog";
+      },
+      error:function(jqXHR, textStatus, errorThrown){
+          console.log(errorThrown)
+          alert("内容获取错误")
+      }
+    });
+});
+
+$(document).on('click','#navindex',function(){
+  $.ajax({
+      url: "http://localhost:5000/",
+      type: "GET",
+      // contentType: 'application/json',
+      // data:"{}",
+      // dataType: "JSON",
+      success:function(data){
+          console.log(data)
+          window.location.href="http://localhost:5000/";
+      },
+      error:function(jqXHR, textStatus, errorThrown){
+          console.log(errorThrown)
+          alert("内容获取错误")
+      }
+    });
+});
+
+$(document).on('click','#navaddblog',function(){
+  $.ajax({
+      url: "http://localhost:5000/addblog",
+      type: "GET",
+      // contentType: 'application/json',
+      // data:"{}",
+      // dataType: "JSON",
+      success:function(data){
+          console.log(data)
+          window.location.href="http://localhost:5000/addblog";
+      },
+      error:function(jqXHR, textStatus, errorThrown){
+          console.log(errorThrown)
+          alert("内容获取错误")
+      }
+    });
+});
